af5da9b099e3794f68aad80360f80ccf910ea87f
Kelooopo <165771312+Kelooopo@users.noreply.github.com>
2025-07-22 03:41:26 +0000

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753155685227

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_99bea0c7._.js",
+  "static/chunks/node_modules_db43f92f._.js"
+],
+    source: "dynamic"
+});
